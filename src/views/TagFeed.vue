ae8c698ca478aa6bf2c7b2274b9edaf022a449f8
Diyorbek <rajabov.diyorbek.it@gmail.com>
2023-01-29 01:25:03 +0500

    some changes

---
+++ src/views/TagFeed.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="home-page">
+    <mcv-banner></mcv-banner>
+    <div class="container page">
+      <div class="row">
+        <div class="col-md-9">
+          <mcv-feed :api-url="apiUrl"></mcv-feed>
+        </div>
+        <div class="col-md-3">
+          <mcv-popular-tags></mcv-popular-tags>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import McvFeed from '@/components/Feed.vue'
+import McvPopularTags from '@/components/PopularTags.vue'
+import McvBanner from '@/components/Banner.vue'
+
+export default {
+  name: 'McvYourFeed',
+  components: {
+    McvFeed,
+    McvPopularTags,
+    McvBanner
+  },
+  computed: {
+    tagName() {
+      return this.$route.params.slug
+    },
+    apiUrl() {
+      return `/articles?tag=${this.tagName}`
+    }
+  }
+}
+</script>
